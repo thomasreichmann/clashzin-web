12627b0d440369a1d19aa9156a9952b7658fd824
Thomas Reichmann <thomasreichmann@hotmail.com>
2021-05-26 13:34:59 -0300

    removed moms

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<img src={a} alt="gostosa" />
+				<h1>EM BREVE UM DOS MELHORES SITES DO BRASIL</h1>
 			</header>
 		</div>
 	);
