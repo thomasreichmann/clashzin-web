2719b0fa374ec8a0152c12790ac001e6e337ef84
Thomas Reichmann <thomasreichmann@hotmail.com>
2021-05-26 13:16:47 -0300

    changed the mom

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,10 @@ import React from 'react';
 import './App.css';
 
 function App() {
-	let a = `https://firebasestorage.googleapis.com/v0/b/clashzin-web.appspot.com/o/miriam.jpg?alt=media&token=7ad283ff-6238-4045-bd38-8b22c52cf8e2`;
+	let a = `https://firebasestorage.googleapis.com/v0/b/clashzin-web.appspot.com/o/4534b07c-83c1-4a20-9aa7-85f11e65aba6.jpg?alt=media&token=5857e93c-8da3-44c6-a48f-f29a9877cf3e`;
 	return (
 		<div className="App">
 			<header className="App-header">
-				<h1>GOSTOSA</h1>
 				<img src={a} alt="gostosa" />
 			</header>
 		</div>
