95ccb79fed4172710977f4f991d2886cf00923de
Thomas Reichmann <thomasreichmann@hotmail.com>
2021-05-26 13:36:30 -0300

    removed unused variable

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 
 function App() {
-	let a = `https://firebasestorage.googleapis.com/v0/b/clashzin-web.appspot.com/o/4534b07c-83c1-4a20-9aa7-85f11e65aba6.jpg?alt=media&token=5857e93c-8da3-44c6-a48f-f29a9877cf3e`;
 	return (
 		<div className="App">
 			<header className="App-header">
