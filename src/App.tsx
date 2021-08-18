fe412b363f7551e5757959b7a17af98605d7a566
thomasreichmann <thomasalmeidar@gmail.com>
2021-08-18 06:10:23 -0300

    added sass to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
+		<div className='App'>
+			<header className='App-header'>
 				<h1>EM BREVE UM DOS MELHORES SITES DO BRASIL</h1>
 			</header>
 		</div>
